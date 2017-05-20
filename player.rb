a3201585003ea8435f3b183e7e140075101c41b3
Jacob Evan Shreve <jacobevanshreve@me.com>
2017-05-20 00:59:25 -0400

    Fix indentation

--- player.rb
+++ player.rb
@@ -1,19 +1,19 @@
 # player.rb
 
 class Player
-    def initialize
-        @x = 1
-        @y = 1
-        @char = '@'
-        @health = 100
-    end
+  def initialize
+    @x = 1
+    @y = 1
+    @char = '@'
+    @health = 100
+  end
 
-    def update
-        # ...
-    end
+  def update
+    # ...
+  end
 
-    def draw
-        draw_text( @x, @y, @char, App.font01, 0xff_DBD9D6)
-        #Gosu::Color.BLACK
-    end
+  def draw
+    draw_text( @x, @y, @char, App.font01, 0xff_DBD9D6)
+    #Gosu::Color.BLACK
+  end
 end
