af9f375d3028b62209b062266520796465300614
Jacob Evan Shreve <jacobevanshreve@me.com>
2017-05-20 01:03:38 -0400

    Add gemfile to document dependencies

--- main.rb
+++ main.rb
@@ -1,7 +1,10 @@
+# Require external gem dependencies
 require 'gosu'
 
+# Add the lib directory next to this file to the list of paths to load from
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/lib'
 
+# Require top-level dependencies of the game
 require 'app'
 
 # begin the fun
